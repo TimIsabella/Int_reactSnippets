b9df3c2ea2aa4c1f9bdb02dccc9e401af5175833
Tim Isabella <timjisabella@gmail.com>
2022-10-05 13:33:31 -0700

    Create toastrOptions.js

---
+++ components/toastrOptions.js
@@ -0,0 +1,19 @@
+const toastrOptions = {
+    closeButton: true,
+    debug: true,
+    newestOnTop: true,
+    progressBar: true,
+    positionClass: 'toast-top-right',
+    preventDuplicates: false,
+    onclick: null,
+    showDuration: '333',
+    hideDuration: '1111',
+    timeOut: '3333',
+    extendedTimeOut: '5555',
+    showEasing: 'swing',
+    hideEasing: 'linear',
+    showMethod: 'fadeIn',
+    hideMethod: 'fadeOut',
+};
+
+export default toastrOptions;
